9bc60b136257968729117fc564de7d05820c1e05
Justin Jones <jus.jones@yahoo.com>
2023-02-05 16:45:23 -0500

    commit tree d0f6fd9c0678bc3da2f6f5f82b27aaf9a11184dd

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+
+#funciton myFunction(param){
+    #console.log("Running myFunction")
+    #return param + 1
+#}
+#const myFunctionReturnValue = myFunction(1);
+#console.log(myFunctionReturnValue)
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Hello, programmer!")
+    puts "Hello, #{name}"
+
+    
